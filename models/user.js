790e3cf382d35f1c6c0e14da628d3efce135c29e
LuaFerGG <luafergg@gmail.com>
2025-05-25 09:01:12 -0500

    feat: enhance user schema with password encryption and comparison methods

--- models/user.js
+++ models/user.js
@@ -1,25 +1,65 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
+
     name: {
         type: String,
         required: true,
     },
+
     email: {
         type: String,
         required: true,
         unique: true,
     },
+
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/.test(value);
+            },
+            message: props => `${props.value} is not a valid password! Must contain at least one lowercase letter, one uppercase letter, one number, one special character, and be at least 8 characters long.`,
+        },
+    },
+
     role: {
         type: String,
         enum: ["student", "teacher"],
         required: true,
     },
+
     cohort: {
         type: String,
         required: false,
     },
+
+    passwordResetToken: String,
+    passwordResetExpires: Date
+
+
 }, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10); // probar con 30 despues 
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.toJSON = function () {
+    const userObject = this.toObject();
+    delete userObject.password;
+    return userObject;
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
+
